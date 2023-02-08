7df29aba27ca3551e17d7627d1fde7c78abcd216
S-moreo <78574820+S-moreo@users.noreply.github.com>
2023-02-08 15:12:15 -0500

    Initial commit

---
+++ src/main/java/net/smoreo/enhancedvillagers/EnhancedVillagersClient.java
@@ -0,0 +1,10 @@
+package net.smoreo.enhancedvillagers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedVillagersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/smoreo/enhancedvillagers/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.smoreo.enhancedvillagers.mixin;
+
+import net.smoreo.enhancedvillagers.EnhancedVilllagers;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EnhancedVilllagers.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
