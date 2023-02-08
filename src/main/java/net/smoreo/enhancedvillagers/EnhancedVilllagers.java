608d4feacd2da0f1ca708556ca390eb6e86dbcb3
S-moreo <78574820+S-moreo@users.noreply.github.com>
2023-02-08 15:13:38 -0500

    Added important comment.

--- src/main/java/net/smoreo/enhancedvillagers/EnhancedVilllagers.java
+++ src/main/java/net/smoreo/enhancedvillagers/EnhancedVilllagers.java
@@ -3,7 +3,7 @@ package net.smoreo.enhancedvillagers;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//Jessie, we need to COOK
 public class EnhancedVilllagers implements ModInitializer {
 	public static final String MOD_ID = "enhancedvillagers";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
